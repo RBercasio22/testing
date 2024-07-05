71ffabe4070d84189b54bee2255e4bba28507d19
Red Hat Guy <RBercasio22@gmail.com>
2024-07-05 21:39:26 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+CORS(app, resources={r"/*":{'origins':"*"}})
+ 
+
+@app.route('/', methods=['GET'])
+def greet():
+    return("Hello")
+
+@app.route('/scan', methods=['GET'])
+def Vue():
+    return("Hello VUE")
+
+if __name__ == "__main__":
+    app.run(debug=True)
